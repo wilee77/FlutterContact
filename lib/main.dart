6592e2daa0f5afb9864b8f1ba888e89074fd9b50
wilee77 <wi.lee@kakao.com>
2023-01-20 23:35:34 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임')
+        ),
+        body:
+            SizedBox(
+              child:
+                ElevatedButton(
+                  child: Text('버튼'),
+                  onPressed: (){},
+                  style: ButtonStyle(),
+                )
+            ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          )
+        ),
+      )
+    );
+  }
+}
